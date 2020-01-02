df02e611b88207466937090400986c75f9d322f3
suki <sukijayasinghe@gmail.com>
2020-01-02 21:15:34 +0800

    find prime number

--- PrimeNumbers/PrimeNumbers.java
+++ PrimeNumbers/PrimeNumbers.java
@@ -1,22 +1,24 @@
 class PrimeNumbers{
     public static void main(String args[]){
         int num = 3;
-        int n = num/2;
+        int n = 0;
         boolean flag = true;
 
-        if(n == 0 || n == 1){
-            System.out.print(n + " is not a prime number");
+        n= num / 2;
+
+        if(num == 0 || num == 1){
+            System.out.println(num + " is not a prime number");
         }else{
-            for(int i = 0; i <= n; i++){
-                if(num%i === 0){
-                    System.out.print(n + " is not a prime number");
+            for(int i = 2; i < n; i++){
+                if(num % i == 0){
+                    System.out.println(num + " is 999 not a prime number");
                     flag = false;
                     break;
                 }
             }
 
             if(flag)
-            System.out.print(n + " is a prime number");
+            System.out.println(num + " is a prime number");
 
         }
 
