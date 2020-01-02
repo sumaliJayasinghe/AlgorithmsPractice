0d5db99f3f64bfe858ab32332584dc4fed3d0341
suki <sukijayasinghe@gmail.com>
2020-01-02 20:51:59 +0800

    find prime number

--- PrimeNumbers/PrimeNumbers.java
+++ PrimeNumbers/PrimeNumbers.java
@@ -2,6 +2,7 @@ class PrimeNumbers{
     public static void main(String args[]){
         int num = 3;
         int n = num/2;
+        boolean flag = true;
 
         if(n == 0 || n == 1){
             System.out.print(n + " is not a prime number");
@@ -9,10 +10,12 @@ class PrimeNumbers{
             for(int i = 0; i <= n; i++){
                 if(num%i === 0){
                     System.out.print(n + " is not a prime number");
+                    flag = false;
                     break;
                 }
             }
 
+            if(flag)
             System.out.print(n + " is a prime number");
 
         }
